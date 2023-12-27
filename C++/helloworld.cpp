b475d9ac14f82fcf14bd866fca3ac42c31ffb5e2
Naman Mittal <namanmittal501@gmail.com>
2023-12-27 08:36:27 +0000

    First_Program

---
+++ C++/helloworld.cpp
@@ -0,0 +1,17 @@
+// #include<cstdio>
+// using namespace std;
+
+// int main(){
+//     puts("Click On Button");
+//     puts("Create A New Player");
+//     puts("Add Life To Player");
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+
+int main(int argc , const char * argv[]){
+    std::cout<<"Hello World!";
+    return 0;
+}
