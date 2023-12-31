3875a5eb185b3e873f9aa8ac5fd9926fb5c6c577
Naman Mittal <namanmittal501@gmail.com>
2023-12-31 08:04:14 +0000

    If...Else

---
+++ C++/01_Basics/program08.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rating = 66;
+
+    if (rating ==7)
+    {
+        puts("5 star rated !");
+    }else if(rating ==66){
+        puts("IDK!");
+    }
+     else {
+        puts("Not 5 star!");
+    }
+
+    if(4){
+        puts("Program ending here!");
+    }
+
+    printf("Your Rating FeedBack is %s \n",rating>444 ? "True" : "False"); //condition ? "true block" : "False block"
+      printf("Your Rating FeedBack is: %s \n",rating ==77 ? "Awesome" : "We will Work On It.");
+    return 0;
+}
