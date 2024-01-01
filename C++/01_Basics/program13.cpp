94ef7d2c1fa7633a24bd4f2dfc688bd8c2dd31e5
Naman Mittal <namanmittal501@gmail.com>
2024-01-01 08:15:35 +0000

    Float_Precision

---
+++ C++/01_Basics/program13.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+
+    printf("Float Size is %ld bits. \n",sizeof(long)*8);
+
+    float my_p_value=20.2;
+    printf("My P value is: %1.3f \n",my_p_value);
+    return 0;
+} 
