08628a5ccaa2d2161b8d171ce4f82bac301c4439
Naman Mittal <namanmittal501@gmail.com>
2024-01-05 08:26:24 +0000

    Basics Of Operations

---
+++ C++/01_Basics/program17.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+        int life = 3;
+        int points = 4;
+         
+         int score =2; // '=' puts right hand side value into the left hand side variable
+
+        
+          score +=points;                   //   score = score + points;  => CODE OPTIMIzATION
+
+            if(life>=0){
+                cout<<score;
+                
+                
+            }   
+
+    return 0;
+}
