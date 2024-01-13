11e9071647676813f3174cee729f34efc77f223d
Naman Mittal <namanmittal501@gmail.com>
2024-01-13 11:58:40 +0000

    Macros

---
+++ C++/01_Basics/program28.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string>
+#define end return 0
+#define endmessage cout<<"Program Ends! \n"
+#define console_log(a) cout << a <<endl
+
+using namespace std;
+
+
+int main(){
+
+    int score=400;
+    // cout<<score<<endl;
+    console_log(score);
+
+int a=4;
+cout<<a<<endl;
+
+string name ="NAMAN";
+console_log(name);
+
+endmessage;
+    end;
+}
