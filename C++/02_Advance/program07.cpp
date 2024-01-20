a9ece4a338ea20ca3d8aa32eb1c8525cc261248d
Naman Mittal <namanmittal501@gmail.com>
2024-01-20 07:41:27 +0000

    "friend" keyword

---
+++ C++/02_Advance/program07.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class Man{
+    string _name;
+    int _age=69;
+    Man(){}
+
+    friend class spiderman;
+
+    protected:
+    Man(const string & name, const int & age)
+    : _name(name),_age(age){}
+    void run(){puts("I can Run");}
+    public:
+    void sayname() const;
+};
+
+void Man::sayname() const {
+    cout<<"My name is "<< _name << endl;
+    cout<<"My age is "<<_age<<endl;
+}
+
+
+//superman
+
+class superman : public Man{
+    bool flight;
+    public:
+    superman(string name) :Man(name,25){}
+    void run(){puts("I can run at Light Speed.");
+    }
+};
+//spiderman
+
+class spiderman : public Man{
+    bool webbing;
+    public:
+    spiderman(string name) :Man(name,19){}
+    void run(){puts("I can run at normal Speed.");
+        cout<<_age<<endl;
+    }
+    
+};
+
+int main()
+{
+
+superman clark("Kent");
+ clark.sayname();
+ clark.run();
+
+spiderman peter("Parker");
+
+peter.sayname();
+peter.run();
+
+
+    return 0;
+}
