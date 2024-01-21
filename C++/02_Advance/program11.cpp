3e6b11423f7f3a4f6d536f054f58a73b7f503ca7
Naman Mittal <namanmittal501@gmail.com>
2024-01-21 08:19:42 +0000

    Shared Pointers In Smart Pointers

---
+++ C++/02_Advance/program11.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<memory>//smart pointer
+using namespace std;
+
+
+class User{
+public:
+    User(){
+        cout<<"User Created."<<endl;
+    }
+      ~User(){
+        cout<<"User Destroyed."<<endl;
+    }
+
+    void testFunc(){
+        cout<<"I Am A Test Function."<<endl;
+    }
+};
+
+int main(){
+
+{
+    // unique_ptr<User> Naman (new User());// non_favourite way of developers
+
+        unique_ptr<User> Naman=make_unique<User>();
+        Naman->testFunc();
+
+        // unique_ptr<User> Naman=Nmn; NOT ALLPWED
+
+}
+
+{
+    // shared_ptr<User>NMN(new User());// wasting memory but allowed
+        shared_ptr<User> NMN = make_shared<User>();
+        shared_ptr<User>NNMMNN=NMN;
+        
+}
+
+//'new' keyword uses Heap memory.
+
+cout<<"OUTSIDE CODE!"<<endl;
+
+
+
+    return 0;
+}
