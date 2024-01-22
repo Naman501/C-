a076e327d079b5d2b58db82e877502cd4eb36d82
Naman Mittal <namanmittal501@gmail.com>
2024-01-22 09:43:48 +0000

    Create, Rename And Delete Files

---
+++ C++/02_Advance/program16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+
+
+    static const char * originalFile ="originalFile.txt";
+    static const char * originalFile ="editedFile.txt";
+
+    // FILE *fh=fopen(originalFile,"w");
+    // fclose(fh);
+    // rename(originalFile ,editedFile);
+    // remove(editedFile);
+
+
+    return 0;
+}
