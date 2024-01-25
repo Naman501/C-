079515368c355b08326a262b75d5a940ba40d150
Naman Mittal <namanmittal501@gmail.com>
2024-01-25 08:39:38 +0000

    Partition And Stable Partition In STL

---
+++ C++/02_Advance/program22.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main(){
+
+
+vector<int> myints ={1,2,3,4,5,6,7,8,9,10};
+
+for(int x: myints){
+    cout <<x<<" ";
+}
+
+cout<<"\n";
+
+
+stable_partition(myints.begin(),end(myints),[](auto x){return x%2==0;});
+
+for(int x: myints){
+    cout <<x<<" ";
+}
+
+cout<<"\n";
+
+
+    return 0;
+}
